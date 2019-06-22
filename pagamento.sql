bba630da4e8ade8f65d04be414b2ba89c6497e41
Rafael Oliveira <38117708+Rafa516@users.noreply.github.com>
2019-06-22 12:21:56 -0300

    Correção

--- pagamento.sql
+++ pagamento.sql
@@ -1,30 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 4.1.14
--- http://www.phpmyadmin.net
---
--- Host: 127.0.0.1
--- Generation Time: 22-Jun-2019 às 04:11
--- Versão do servidor: 5.6.17
--- PHP Version: 5.5.12
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- Database: `bdcadastropagamento`
---
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `pagamento`
---
 
 CREATE TABLE IF NOT EXISTS `pagamento` (
   `Nome` varchar(35) DEFAULT NULL,
@@ -33,6 +6,4 @@ CREATE TABLE IF NOT EXISTS `pagamento` (
   `Ano` int(10) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
